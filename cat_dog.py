ce3899e3b4b6a2dd3895d4227f0a837f5b378c58
caokj <1406607599@qq.com>
2023-04-28 16:45:16 +0800

    20230428

--- cat_dog.py
+++ cat_dog.py
@@ -1,20 +1,45 @@
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+# 数据集预处理
+"""
+数据来源
+知乎https://zhuanlan.zhihu.com/p/388676784
+
+现在目录结构：
+root='E:\\kagglecatsanddogs\\'
+cat = root + 'PetImages\\Cat'
+dog = root + 'PetImages\\Dog'
+
+目标结构：
+'data\cad\\train\dog'
+'data\cad\\train\cat'
+
+'data\cad\\test\dog'
+'data\cad\\test\cat'
+
 
 """
-知乎https://zhuanlan.zhihu.com/p/388676784
-"""
+def preDealData():
+
+    pass
+
 # 自定义加载数据集
 class Dc_DataLoader(Dataset):
-    def __init__(self):
+    def __init__(self, root, is_train=True):
         super().__init__()
+        # 数据集
+        self.dataset = []
+
+        train_or_set = 'train' if is_train else 'test'
+        # 从路径加载数据
+
 
     def __getitem__(self, item):
         pass
 
     def __len__(self):
-        pass
+        return len(self.dataset)
 
 
 
